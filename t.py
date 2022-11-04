12b5adb9013e9852ca8e9aa9fb0bb3832a08ce6e
Pécsi Bálint <pecsi.balint@students.jedlik.eu>
2022-11-04 12:27:30 +0100

    readme file

---
+++ t.py
@@ -0,0 +1,17 @@
+class asd:
+    name: str
+    attr1: str
+    mana: int
+    attr3: str
+    attr4: str
+
+cards = {0:(), 1:()#...
+         #......
+        } 
+
+player = 0 # player = 1
+hands = [
+    [],
+    []
+    ]
+
