dda96b31f96141277f443117d74a5d7128e8967c
Balint837 <pecsi.balint@students.jedlik.eu>
2022-11-11 10:06:12 +0100

    import script + default parameters

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
 class Card {
-    constructor(atk, def, mana, on_turnend, on_death, on_play, on_attack, is_taunt, instant_atk, on_spell, show_stats){
-        // onturnend --> function
-        // ondeath --> function
-        // onplay (place) --> function
-        // onattack --> function
-        // is_taunt --> bool
-        // instant_attack --> bool
-        // onspell --> function()
-        // atk --> int
-        // def --> int
-        // mana --> int
-        // show_stats --> bool
-        this.atk = atk
-        this.def = def
-        this.mana = mana
-        this.on_turnend = on_turnend
-        this.on_death = on_death
-        this.on_play = on_play
-        this.on_attack = on_attack
-        this.is_taunt = is_taunt
-        this.instant_atk = instant_atk
-        this.on_spell = on_spell
-        this.show_stats = show_stats
+    constructor(atk, def, mana_cost, on_turnend=()=>{}, on_death=()=>{}, on_play=()=>{}, on_attack=()=>{}, on_spell=()=>{}, is_taunt=false, instant_atk=false, show_stats=true){
+        this.atk = atk // int
+        this.def = def // int
+        this.mana_cost = mana_cost // int
+        this.on_turnend = on_turnend // function
+        this.on_death = on_death // function
+        this.on_play = on_play // function
+        this.on_attack = on_attack // function
+        this.on_spell = on_spell // function
+        this.is_taunt = is_taunt // bool
+        this.instant_atk = instant_atk // bool
+        this.show_stats = show_stats // bool
     }
 };
-
-
-()=>{};
