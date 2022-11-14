0f35ee9f0ba419ca60cef4e0553dae17cf578acd
Szaloki Peter <szaloki.peter.krisztian@students.jedlik.eu>
2022-11-14 09:05:57 +0100

    kill me rn

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 class Card {
-    constructor(atk=0, def=0, mana_cost, on_play=(card)=>{}, on_turnend=(card)=>{}, on_death=(card)=>{}, on_attack=(card)=>{}, on_spell=(card)=>{}, on_damage=(card)=>{}, is_taunt=false, instant_atk=false, show_stats=true){
+    constructor(atk=0, def=0, mana_cost, on_play=[], on_turnend=[], on_death=[], on_attack=[], on_spell=[], on_damage=[], is_taunt=false, instant_atk=false, is_spell=false){
         this.atk = atk; // int
         this.def = def; // int
         this.mana_cost = mana_cost; // int
-        this.on_play = on_play; // function
-        this.on_turnend = on_turnend; // function
-        this.on_death = on_death; // function
-        this.on_attack = on_attack; // function
-        this.on_spell = on_spell; // function
-        this.on_damage = on_damage; // function
+        this.on_play = on_play; // list of functions
+        this.on_turnend = on_turnend; // list of functions
+        this.on_death = on_death; // list of functions
+        this.on_attack = on_attack; // list of functions
+        this.on_spell = on_spell; // list of functions
+        this.on_damage = on_damage; // list of functions
         this.is_taunt = is_taunt; // bool
         this.instant_atk = instant_atk; // bool
-        this.show_stats = show_stats; // bool
+        this.is_spell = is_spell; // bool
     }
 };
 
@@ -22,24 +22,39 @@ class Hero{
     /*hero_power is prob an instance for now, can't think of anything yet*/
 }
 
+
+function damage1AnyMinion(targetId, atk){}
+function damage1AnyObj(targetId, atk){}
+function returnToDeck(targetId){}
+function summonCard(card_idx)
+
+new Card(
+    mana_cost=2,
+    on_play=[["target.id", "this.atk"], damage1AnyObj], [["self.id"], returnToDeck],
+    show_stats = true
+    )
+
 let max_id = 0;
 let id_dict = {};
-let card_types = [new Card(mana_cost=max_mana+1)];
+let card_types = [
+    new Card(mana_cost=max_mana+1),
+    
+];
 let heroes = []
 let weapons = [0, 0]
 
 class PlacedCard{
     constructor(card_idx) {
         this.id = max_id++;
-        this.card = card_idx;
-        this.hp = this.get_card().def
-        this.atk = this.get_card().atk
-        this.may_attack = this.get_card().instant_atk
-        this.get_card().on_play()
+        this.card_idx = card_idx;
+        this.card = this.get_card_type()
+        this.hp = this.card.def
+        this.atk = this.card.atk
+        this.may_attack = this.card.instant_atk
     }
 
-    get_card() {
-        return card_types[this.card];
+    get_card_type() {
+        return card_types[this.card_idx];
     }
 }
 new PlacedCard(0)
