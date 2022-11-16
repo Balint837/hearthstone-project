1662d24b6563cb3f42df8d17f54eaf0fe5f58d02
Balint837 <pecsi.balint@students.jedlik.eu>
2022-11-16 20:13:15 +0100

    why do i have to commit again?

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 class Card {
-    constructor(atk=0, def=0, mana_cost, on_play=(card)=>{}, on_turnend=(card)=>{}, on_death=(card)=>{}, on_attack=(card)=>{}, on_spell=(card)=>{}, on_damage=(card)=>{}, is_taunt=false, instant_atk=false, show_stats=true){
+    constructor(atk=0, def=0, mana_cost, on_play=[], on_turnend=[], on_death=[], on_attack=[], on_spell=[], on_damage=[], is_taunt=false, instant_atk=false, show_stats=true){
+        // default lambdas: (card)=>{}
         this.atk = atk; // int
         this.def = def; // int
         this.mana_cost = mana_cost; // int
-        this.on_play = on_play; // function
-        this.on_turnend = on_turnend; // function
-        this.on_death = on_death; // function
-        this.on_attack = on_attack; // function
-        this.on_spell = on_spell; // function
-        this.on_damage = on_damage; // function
+        this.on_play = on_play; // list of functions
+        this.on_turnend = on_turnend; // list of functions
+        this.on_death = on_death; // list of functions
+        this.on_attack = on_attack; //  list of functions
+        this.on_spell = on_spell; //  list of functions
+        this.on_damage = on_damage; //  list of functions
         this.is_taunt = is_taunt; // bool
         this.instant_atk = instant_atk; // bool
         this.show_stats = show_stats; // bool
@@ -18,15 +19,25 @@ class Card {
 
 
 class Hero{
-    constructor(hero_power, hp)
+    constructor(hero_power=(target)=>{}){
+        this.hp = 30
+    }
     /*hero_power is prob an instance for now, can't think of anything yet*/
 }
 
-let max_id = 0;
-let id_dict = {};
-let card_types = [new Card(mana_cost=max_mana+1)];
-let heroes = []
-let weapons = [0, 0]
+
+let max_hand = [10, 10] // 
+let max_id = 0; // DON'T YOU DARE FUCKING TOUCH THIS
+let id_dict = {}; // integers/ids
+let card_types = [new Card(mana_cost=max_mana+1)]; // cards
+let hero_types = [] // heroes
+let heroes = [0, 0] // indexes
+let weapons = [0, 0] // indexes
+
+let spent_corpses = [0, 0]
+let corpses = [0, 0]
+
+last_spell = 0
 
 class PlacedCard{
     constructor(card_idx) {
@@ -35,11 +46,14 @@ class PlacedCard{
         this.hp = this.get_card().def
         this.atk = this.get_card().atk
         this.may_attack = this.get_card().instant_atk
-        this.get_card().on_play()
     }
 
     get_card() {
         return card_types[this.card];
     }
 }
+
+
+
+
 new PlacedCard(0)
